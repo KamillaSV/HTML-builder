6bb7a93d39e299e4bede6928b3bc00dd0791c6a1
KamillaSV <kamilo4ka.92@mail.ru>
2022-05-27 04:21:36 +0500

    feat: fix 2 & 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+
+fs.readdir((path.join(__dirname,'secret-folder')), (err, files) => {
+    if (err) throw err;
+    else { 
+        files.forEach((file) => {
+             if (file.isFile()) {
+                let fileName = path.parse(files[i]).name;
+                fs.stat(path.join(path.join(__dirname,'secret-folder'))), (err, stats) => {
+                    if (err) throw err;
+                    else {
+                        let fileSize = `${(stats.size/1024).toFixed}kb`
+                        let fileExt = path.extname((path.join(__dirname,'secret-folder'))).slice(1);
+                        console.log(fileName,fileExt,fileSize);
+                    }
+                }
+            }
+            else { throw err}
+        });
+    }
 });
